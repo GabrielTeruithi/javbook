e0b3abc6dab987ca331848a71ddd9ea9c2428f73
gTeruithi <gabrielt.oguma@gmail.com>
2024-05-03 10:58:41 -0300

    Create CodeCell for muliple windows Added react-resizable

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
-import  'bulmaswatch/darkly/bulmaswatch.min.css'
-import {useState, useRef} from "react";
-import CodeEditor from "./components/code-editor";
-import Preview from "./components/preview";
-import bundler from "./bundler";
-function App() {
-  const ref = useRef<any>();
-  const [code, setCode] = useState("");
-  const [input, setInput] = useState("");
+import 'bulmaswatch/darkly/bulmaswatch.min.css'
+import CodeCell from "./components/code-cell";
 
-  const onClick = async () => {
-    const output = await bundler(input);
-    setCode(output);
-  };
-
+const App = () => {
   return (
-    <div>
-      <CodeEditor
-        initialValue="const a = 1;"
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
-    </div>
+    <>
+      <CodeCell/>
+      <CodeCell/>
+    </>
   );
 };
 
